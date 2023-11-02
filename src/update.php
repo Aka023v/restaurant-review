9ffdc0d52454375dbe1bcc4d77038e632272411d
Aka023v <148590386+Aka023v@users.noreply.github.com>
2023-11-02 18:52:45 +0000

    修正

--- src/update.php
+++ src/update.php
@@ -2,14 +2,14 @@
 $id = filter_input(INPUT_POST, 'id');
 $poster = filter_input(INPUT_POST, 'poster');
 $restaurant_name = filter_input(INPUT_POST, 'restaurant_name');
-$tweet = filter_input(INPUT_POST, 'tweet');
+$review = filter_input(INPUT_POST, 'review');
 
 $dbh = new PDO(
   'mysql:dbname=restaurant_review;host=mysql;charset=utf8',
   'root',
   'password'
 );
-$query = "UPDATE tweet SET poster='$poster', restaurant_name='$restaurant_name', tweet='$tweet' WHERE id = '$id'";
+$query = "UPDATE review SET poster='$poster', restaurant_name='$restaurant_name', review='$review' WHERE id = '$id'";
 $dbh->query($query);
 
 header('Location: ./index.php');
